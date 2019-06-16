87f9d7dbdc6beccce0677280734f4b90ed0c2689
Mohamad Ibnu Rizky <mohamadibnu.r@gmail.com>
2019-06-17 06:37:58 +0700

    update funtion loadLibrary()

--- application/helpers/load_helper.php
+++ application/helpers/load_helper.php
@@ -29,6 +29,11 @@
 		$ci->load->helpers($helper);
 	}
 
+	function loadlibrary($library){
+		$ci = &get_instance();
+		$ci->load->library($library);
+	}
+
 	function inPost($name){
 		$ci = &get_instance();
 		return $ci->input->post($name);
